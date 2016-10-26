452a0b6ce3285f75c84c56fa299e7d176b4622a6
Philip Chung <philarlol@gmail.com>
2016-10-25 17:15:34 -0700

    Refactored promise call chain into thunks that takes option as a parameter

--- src/GoogleMapsInstance.js
+++ src/GoogleMapsInstance.js
@@ -6,7 +6,7 @@ let DomElement;
 let geoResult;
 
 // Lazy load Google Maps JS API 
-const loadMapsApi =(apiKey) => {
+const loadMapsApi = (opt) => {
 
 	if (window.google) return Promise.resolve();
 
@@ -15,50 +15,62 @@ const loadMapsApi =(apiKey) => {
 		script.type = 'text/javascript';
 		script.onload = resolve;	
 		document.getElementsByTagName('head')[0].appendChild(script);
-		script.src = `http://maps.googleapis.com/maps/api/js?v=3&key=${apiKey}`;
+		script.src = `http://maps.googleapis.com/maps/api/js?v=3&key=${opt.key}`;
 	});
+}
+
+// Reverse Geocoding (Address Lookup)
+const getGeolocation = (opt) => () => {
+
+	if (geoResult) return Promise.resolve(geoResult);
+
+	return new Promise((resolve, reject) => {
+
+		const latlng = { 
+			lat: parseFloat(opt.location[0]), 
+			lng: parseFloat(opt.location[1])
+		}
+
+ 		const geocoder = new google.maps.Geocoder;
+		geocoder.geocode({ 'location': latlng }, (results, status) => {
+			if (status === 'OK') {
+				geoResult = results;
+				resolve(results);
+			}
+		});
+	}) 
+}
+
+// Create Map element and return results
+const createMapElement = (opt) => (geoResult) => {
+
+	const mapOptions = {
+	    ...opt,
+	    center: new google.maps.LatLng(opt.location[0], opt.location[1]),  
+	};
+
+	const newElement = document.createElement('div');
+	MapInstance = new google.maps.Map(newElement, mapOptions);
+	DomElement = newElement;
+
+	return {
+		dom: DomElement,
+		geo: geoResult
+	};
 }
 
 // Return reference to DOM element or create it with Google Maps
 const getDomElement = (opt) => {
 
-	var latlng = {lat: parseFloat(opt.location[0]), lng: parseFloat(opt.location[1])};
-
 	if (!DomElement) {
-		return loadMapsApi(opt.key)
 
-		// Reverse Geocoding (Address Lookup)
-		.then(() => {
- 			 const geocoder = new google.maps.Geocoder;
- 			 return new Promise((resolve, reject) => {
-				geocoder.geocode({ 'location': latlng }, function(results, status) {
-					if (status === 'OK') {
-						geoResult = results;
-						resolve(results);
-					}
-				});
- 			}) 
-		})
-		// Create Map element
-		.then((result) => {
-
-			const mapOptions = {
-			    ...opt,
-			    center: new google.maps.LatLng(opt.location[0], opt.location[1]),  
-			};
-
-			const newElement = document.createElement('div');
-			MapInstance = new google.maps.Map(newElement, mapOptions);
-			DomElement = newElement;
-
-			return {
-				dom: DomElement,
-				geo: geoResult
-			};
-		});
+		return loadMapsApi(opt)
+			.then(getGeolocation(opt))
+			.then(createMapElement(opt))
 	}
 	else {
-		return  Promise.resolve({
+		
+		return Promise.resolve({
 				dom: DomElement,
 				geo: geoResult
 			});
